7ead5e1b5ad5e8086ba3d31bf64d036fd92d8698
asimplefish <liyufeng854@gmail.com>
2026-03-07 03:00:00 +0800

    Updates

--- assets/ChatView-TyXStZUl.js
+++ assets/ChatView-TyXStZUl.js
@@ -1,4 +1,4 @@
-import{o,c as n,F as $,p as T,n as C,a as t,t as g,f as z,w as U,v as E,b as P,D as R,r as h,E as M,G as D,g as k,j as w,h as F,d as A,i as H,q as I,A as O}from"./index-DxCcE3yx.js";const b=`【两人基本信息】
+import{o,c as n,F as $,p as T,n as C,a as t,t as g,f as z,w as U,v as E,b as P,D as R,r as h,E as M,G as D,g as k,j as w,h as F,d as A,i as H,q as I,A as O}from"./index-DaGJXOUq.js";const b=`【两人基本信息】
 小鱼（男）：在北京工作，之前浙大，目标是当大学老师，热情主动，情绪稳定有仪式感。微信名"兄弟"。
 梓樱（女，陈梓樱）：上海交通大学研究生，化学/生物方向，日常是采样、跑数据、写论文、开组会。i人，享受独处，理性善良果断。
 
